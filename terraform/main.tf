e50f93551fe3124ba30c2c1fc1fbe68fc71d7c3f
Antonio Cuenco <acuenco@atlascarbon.ag>
2024-08-26 00:00:09 +1000

    fix: back to eks cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
-    }
   }
 }
 
@@ -157,72 +153,4 @@ module "eks" {
     Environment = var.environment
     Terraform   = "true"
   }
-}
-
-###############################################################################
-# EBS CSI
-###############################################################################
-module "ebs_csi_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.44.0"
-
-  role_name = "${var.app_name}-ebs-csi"
-
-  attach_ebs_csi_policy = true
-  ebs_csi_kms_cmk_ids   = [module.kms.key_arn]
-
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-
-  tags = {
-    Environment = var.environment
-    Terraform   = "true"
-  }
-}
-
-data "aws_eks_addon_version" "ebs_csi" {
-  addon_name         = "aws-ebs-csi-driver"
-  kubernetes_version = "1.30"
-  most_recent        = true
-}
-
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "aws-ebs-csi-driver"
-
-  addon_version               = data.aws_eks_addon_version.ebs_csi.version
-  resolve_conflicts_on_update = "PRESERVE"
-  service_account_role_arn    = module.ebs_csi_irsa_role.iam_role_arn
-
-  tags = {
-    Environment = var.environment
-    Terraform   = "true"
-  }
-}
-
-###############################################################################
-# Storage Class
-###############################################################################
-resource "kubectl_manifest" "ebs_csi_default_storage_class" {
-  yaml_body = <<-YAML
-  apiVersion: storage.k8s.io/v1
-  kind: StorageClass
-  metadata:
-    annotations:
-      storageclass.kubernetes.io/is-default-class: "true"
-    name: gp3-default
-  provisioner: ebs.csi.aws.com
-  reclaimPolicy: Delete
-  volumeBindingMode: WaitForFirstConsumer
-  allowVolumeExpansion: true
-  parameters:
-    type: gp3  
-    fsType: ext4
-    encrypted: "true"
-    kmsKeyId: "arn:aws:kms:us-east-1:770236806245:key/30d24e00-0d3b-417f-8bfc-b7416d8a4703"
-  YAML
 }
