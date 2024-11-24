cf056ca9d113527fd127bc5510e00d5ed18ad73d
Ibukun Soetan <160017075+h3llo-world-33@users.noreply.github.com>
2024-11-24 15:20:56 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-     kubectl = {
+    kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14"
     }
